e73266f36f264c04d764b908a2c2fdd1d96b3374
Alec Robinson <s4203312@glos.ac.uk>
2023-11-08 11:40:23 +0000

    working some more

--- tabupython/main_tabu.py
+++ tabupython/main_tabu.py
@@ -2,116 +2,46 @@ import turtle
 import random
 import math
 import sys
-#
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-import os
-
-df_cities = pd.read_csv("cities.csv")
-df_cities.head()
-df_cities = df_cities.iloc[:500]
-
-citys_no_header = pd.read_csv("cities.csv",  usecols=[1,2] ,header=None, skiprows=1)
-sectionOfCities = citys_no_header.iloc[:500]
-
-#fig = plt.figure(figsize=(20,20))
-#cmap, norm = from_levels_and_colors([0.0, 0.5, 1.5], ['red', 'black'])
-#plt.scatter(df_cities['X'],df_cities['Y'],marker = '.',c=(df_cities.CityId != 0).astype(int), cmap='Set1', alpha = 0.6, s = 500*(df_cities.CityId == 0).astype(int)+1)
-#plt.show()
-
-#
+# if RNG seed is None, defaults to system time
 rng_seed = None
-number_iterations = 10
-number_cities = len(df_cities)
-#tabu_length = 3
-#tabu_list = []
+number_iterations = 50
+number_cities = 30
+tabu_length = 3
+tabu_list = []
+
 
 def main():
-    
+    # Just to be sure we don't accidentally
+    # create a new rng_seed if we edit code later
     global rng_seed
 
+    # Sets globals according to command line args
     get_arguments()
 
     # Could add a fixed seed for debugging
     random.seed(rng_seed)
     citylocations = []
 
-    for i in range(0,number_cities):
-        print(sectionOfCities.iat[i,0])
-        citylocations.append((float(sectionOfCities.iat[i,0]), float(sectionOfCities.iat[i,1])))    #Manipulating the data to fit the screen(must remove before submition)
+    # Generating the Cities
+    for i in range(0, number_cities):
+        citylocations.append(
+            (random.randint(-299, 299),
+             random.randint(-299, 299)))
 
-    #Setting up the turtle screen
+    # Setting up the screen (for turtle)
     screen = turtle.Screen()
-    turtle.speed(0)     #Turns animation off so turtle can go as fast as possible
-    screen.setup(width=1000,height=1000)
-    screen.title('Traveling Santa')
+    turtle.speed(0)
+    screen.setup(600, 600)
 
-    #dotCities(citylocations)                #Draws out all cities
-    #drawpath(citylocations)                #Draws out all cities connected
-    
-    # Run 50 iteration of Gradient Descent search
+    # Run 50 iterations of TABU search
     for i in range(0, number_iterations):
-        citylocations = gd_iteration(citylocations)
+        citylocations = tabu_iteration(citylocations)
 
-    #tabu_iteration(citylocations)           #Running the pathfinding method
-
+    # Pause turtle until clicked
     screen.exitonclick()
 
-def get_arguments():
-    global number_cities
-    global number_iterations
-    global rng_seed
 
-    # If no command line arguments supplied then
-    # default and return function
-    if len(sys.argv) == 1:
-        print(f"Usage: {sys.argv[0]} <number of cities> <number of "
-              "iterations> <rng seed>")
-        print(f"Defaulting to:\nnumber of cities={number_cities},"
-              f"\nnumber of iterations={number_iterations},"
-              f"\nrng seed={rng_seed}")
-        return
-
-    # If they have been supplied but cause an error
-    # (are incorrect) print help and exit
-    try:
-        number_cities = int(sys.argv[1])
-        number_iterations = int(sys.argv[2])
-        rng_seed = int(sys.argv[3])
-    except:
-        try:
-            number_cities = int(sys.argv[1])
-            number_iterations = int(sys.argv[2])
-        except:
-            raise SystemExit(f"Usage: {sys.argv[0]}"
-                             "<number of cities> <number of iterations>"
-                             "<rng seed (optional)>")
-
-
-def gd_iteration(citylocations):
-    # Create a list of candidates
-    candidates = create_candidates(citylocations)
-
-    # Get a sorted list of candidates
-    # lowest (i.e. best) first
-    scored_candidates = sorted(candidates, key=objective_function)
-
-    # The best scored candidate is our choice
-    best_candidate = scored_candidates[0]
-
-    # Draw its path, and return it
-    drawpath(best_candidate)
-    return best_candidate
-
-
-
-
-
-
-'''
 def get_arguments():
     global number_cities
     global number_iterations
@@ -203,12 +133,13 @@ def tabu_check(candidate):
 
     # Return the flag value
     return usable
-'''
 
 
 def objective_function(candidate):
+    # Sum set initially to 0
     sum = 0
 
+    # For each city pair,
     for i in range(0, number_cities):
         if i == number_cities - 1:
             sum = sum + euclidean_distance(candidate[-1], candidate[0])
@@ -217,10 +148,12 @@ def objective_function(candidate):
 
     return sum
 
+
 def euclidean_distance(pointa, pointb):
     return math.sqrt(
-        math.pow(pointa[0] - pointb[0], 2) + 
+        math.pow(pointa[0] - pointb[0], 2) +
         math.pow(pointa[1] - pointb[1], 2))
+
 
 def create_candidates(citylocations):
     candidates = []
@@ -260,18 +193,6 @@ def drawpath(cities):
 
     turtle.goto(cities[0])
 
-'''
-def dotCities(cities):
-    turtle.clear()
-    turtle.penup()
-
-    for city in cities:
-        print(city)
-        turtle.goto(city)
-        turtle.dot(5)
-
-    turtle.goto(cities[0])
-'''
 
 def drawdistance(cities):
     turtle.penup()
@@ -279,6 +200,7 @@ def drawdistance(cities):
     distance = objective_function(cities)
     turtle.write(f'Distance: {distance}', move=False,
                  align='left', font=('Arial', 8, 'normal'))
+
 
 if __name__ == '__main__':
     main()
